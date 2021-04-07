c126cf7bad11c3e0db2127e15fcfcf655ed14b9b
Nathan Brooks <nathanbrooks@Nathans-MacBook-Air.local>
2021-04-07 00:29:22 -0400

    finished webhook

---
+++ README.md
@@ -0,0 +1,10 @@
+to run the flask app first:
+$ export FLASK_APP=app.py
+
+then to run type:
+$ flask run
+
+
+to enter debug mode:
+$ export FLASK_ENV=development
+$ flask run
---
+++ app.py
@@ -0,0 +1,56 @@
+# save this as app.py
+import json, config
+from flask import Flask, escape, request
+from binance.client import Client
+from binance.enums import *
+
+app = Flask(__name__)
+
+client = Client(config.BINANCE_API_KEY, config.BINANCE_SECRET_KEY, tld='us')
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        #order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity, time=time)
+        order = client.create_test_order(symbol="ETHUSDT", side=side, type=order_type, quantity=quantity)
+        print(order)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return order
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = json.loads(request.data)
+
+    passphrase = data['passphrase']
+    if( config.WEBHOOK_PASSPHRASE != passphrase):
+        return {
+            "code": "error",
+            "message": "invalid credentials"
+        }
+
+    print(data['ticker'])
+    print(data['bar'])
+
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['order_contracts']
+    order_response = order(side=side, quantity=quantity, symbol="ETHUSDT")
+
+    if order_response:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+        return {
+            "code": "error",
+            "message": "order failed"
+        }
